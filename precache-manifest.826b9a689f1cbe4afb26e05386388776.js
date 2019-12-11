3b7779e63187b0171ff4ffe783aafff8b67acc19
Espen Sørby <espen@sorby.net>
2019-12-11 16:32:54 +0100

    Updates

---
+++ precache-manifest.826b9a689f1cbe4afb26e05386388776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4fb7b36aa1a06e66d238bdc719ba09",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "95ad6351068c2b44d8a2",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a5c483abff514890a68",
+    "url": "/test/static/js/2.e34603ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.e34603ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95ad6351068c2b44d8a2",
+    "url": "/test/static/js/main.0c1d95cb.chunk.js"
+  },
+  {
+    "revision": "f5855940c06bc3d17424",
+    "url": "/test/static/js/runtime-main.892c12c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
